d55a095222738d689d69e286d6a30ee3af853154
Elijah Whitaker <Eli2257251@maricopa.edu>
2023-12-07 22:18:39 -0700

    Adding gnome and cocktail shaker code

--- src/main/java/com/example/sorting/SortingUtility.java
+++ src/main/java/com/example/sorting/SortingUtility.java
@@ -4,7 +4,7 @@ public class SortingUtility {
 
 
     public static <T extends Comparable<T>> void gnomeSort(T[] data) {
-           // TODO implement Gnome Sort here
+        // TODO implement Gnome Sort here
         int pos = 0;
         int[] a = new int[0];
         while (pos < a.length) {
@@ -21,37 +21,61 @@ public class SortingUtility {
     }
 
 
-        
-    public static <T extends Comparable<T>> void cocktailShakerSort(T[] data){
+    public static <T extends Comparable<T>> void cocktailShakerSort(T[] data) {
 
-            // TODO implement Cocktail Shaker Sort here
+            // TODO implement Cocktail Shaker Sort here\
+        int[] a = new int[0];
+            boolean swapped;
+            do {
+                swapped = false;
 
-        int position, scan;
-
-        for (position = data.length - 1; position >= 0; position--) {
-
-            for (scan = 0; scan <= position - 1; scan++) {
-
-                if (data[scan].compareTo(data[scan + 1]) > 0) {
-                    swap(data, scan, scan + 1);
+                // Forward pass
+                for (int i = 0; i < a.length - 1; i++) {
+                    if (a[i] > a[i + 1]) {
+                        // Swap A[i] and A[i + 1]
+                        int temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        swapped = true;
+                    }
                 }
-            }
+
+                if (!swapped) {
+                    // Exit the outer loop if no swaps occurred.
+                    break;
+                }
+
+                swapped = false;
+
+                // Backward pass
+                for (int i = a.length - 2; i >= 0; i--) {
+                    if (a[i] > a[i + 1]) {
+                        // Swap A[i] and A[i + 1]
+                        int temp = a[i];
+                        a[i] = a[i + 1];
+                        a[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+            } while (swapped); // If no elements have been swapped, then the list is sorted
+        }
+
+
+        public static <T extends Comparable<T>>void shellSort (T[]data){
+
+            // TODO implement Shell Sort here
+        }
+
+        private static <T extends Comparable<T>>void swap (T[]data,int index1, int index2){
+
+            T temp = data[index1];
+            data[index1] = data[index2];
+            data[index2] = temp;
+
         }
     }
 
-            public static <T extends Comparable<T>>void shellSort (T[]data) {
-
-                // TODO implement Shell Sort here
-            }
-
-            private static <T extends Comparable<T>>void swap (T[]data,int index1, int index2){
-
-                T temp = data[index1];
-                data[index1] = data[index2];
-                data[index2] = temp;
-
-            }
-        }
 
 
 
